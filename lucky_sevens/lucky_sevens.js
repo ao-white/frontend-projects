3fd866c01e3cb684b41083b32db60d97d22463ff
aowhite <aowhite@ncsu.edu>
2019-11-01 22:08:02 -0400

    Added Lucky Sevens dice game

---
+++ lucky_sevens/lucky_sevens.js
@@ -0,0 +1,54 @@
+function rollDice(){
+	return Math.floor(Math.random() * 6) + 1;
+}
+
+function luckySevens(){
+	var initBet = (document.forms["betForm"]["bet"].value);
+	var betTotal = parseFloat(initBet);
+	var maxWon = 0;
+	var maxRolls = 0;
+	var diceSum = 0;
+	var rollCount = 0;
+	
+	if (initBet == "" || isNaN(initBet)) {
+        alert("Please enter only a number.");
+        document.forms["betForm"]["initBet"]
+           .parentElement.className = "form-group has-error";
+        document.forms["betForm"]["initBet"].focus();
+        return false;
+    }
+	else if (initBet <= 0){
+		alert("Starting Bet must be greater than 0.");
+        document.forms["betForm"]["initBet"]
+           .parentElement.className = "form-group has-error";
+        document.forms["betForm"]["initBet"].focus();
+        return false;
+	}
+	
+	for(var i = 0; betTotal > 0; i++){
+			diceSum = rollDice() + rollDice();
+			console.log("sum: " + diceSum);
+			rollCount++;
+			console.log("roll: " + rollCount);
+			if(diceSum === 7){
+				betTotal += 4;
+				if(betTotal > maxWon){
+					maxWon = betTotal;
+					maxRolls = rollCount;
+				}
+			}
+			else{
+				betTotal--;
+			}
+			console.log("maxRoll: " + maxRolls);
+			console.log("amount: " + betTotal);
+	}
+	
+	document.getElementById("results").style.display = "block";
+	document.getElementById("submit").innerText = "Play Again";
+	document.getElementById("initBet").innerText = "$"+ parseFloat(initBet).toFixed(2);
+	document.getElementById("rollsBroke").innerText = rollCount;
+	document.getElementById("highestWon").innerText = "$"+ maxWon.toFixed(2);
+	document.getElementById("rollsHighest").innerText = maxRolls;
+	return false;
+}
